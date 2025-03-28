b204de72b2f3dd72446502fcfda13390ea1f10b6
Akib306 <123131218+Akib306@users.noreply.github.com>
2025-03-28 06:32:16 -0600

    set up tailwind 3.4.17

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,9 @@ function App() {
 
   return (
     <>
-      <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-            <h1>Welcome to DevTalk 💬</h1>
-            <p>A channel-based Q&A tool for programmers, powered by React and Node.js.</p>
-            <p>Register or log in to ask questions, get help, and contribute.</p>
-        </div>
+      <h1 className="text-3xl font-bold underline italic">
+        Hello world!
+      </h1>
     </>
   )
 }
